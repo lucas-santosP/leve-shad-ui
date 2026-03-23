1992a71974a02139fc4ccc22d9218ec83810b180
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2026-03-23 16:18:33 -0300

    fix: input testing

--- src/components/input/input.test.tsx
+++ src/components/input/input.test.tsx
@@ -12,4 +12,14 @@ describe("Input", () => {
     const { getByPlaceholderText } = render(<Input placeholder="Type here" />);
     expect(getByPlaceholderText("Type here")).toBeTruthy();
   });
+
+  it("renders with left icon", () => {
+    const { getByTestId } = render(<Input leftIcon={<div data-testid="left-icon" />} />);
+    expect(getByTestId("left-icon")).toBeTruthy();
+  });
+
+  it("renders with right icon", () => {
+    const { getByTestId } = render(<Input rightIcon={<div data-testid="right-icon" />} />);
+    expect(getByTestId("right-icon")).toBeTruthy();
+  });
 });
